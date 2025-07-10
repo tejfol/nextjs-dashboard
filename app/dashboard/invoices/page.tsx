1b2edc3d860c66dd1ec00846d59982c75af821d1
Matei Mark <mateimarklukas@gmail.com>
2025-07-10 16:19:50 +0000

    Completed Chapter 4

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-3xl font-bold">Welcome to Invoices</h1>
+        <p className="text-lg text-gray-600">
+          This is the example page for the invoices.
+        </p>
+      </div>
+    </div>
+  );
+}
